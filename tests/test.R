fdfc63d7dfa574ee19aed9805bb703c1e8eecf4f
Paul Murrell <paul@stat.auckland.ac.nz>
2018-05-23 13:30:00 +1200

    added another test for rotated rasterization

--- tests/test.R
+++ tests/test.R
@@ -16,3 +16,11 @@ grid.circle(r=.3, name="c")
 grid.rect(x=grobX("c", 0), just="left",
           width=.2, height=.3, name="r")
 grid.rasterize("r|c", grep=TRUE, global=TRUE, merge=TRUE)
+
+## Rasterize in rotated viewport
+grid.newpage()
+pushViewport(viewport(width=.5, height=.5, angle=30))
+grid.rect()
+grid.text("test", name="t")
+grid.rasterize("t")
+upViewport()
